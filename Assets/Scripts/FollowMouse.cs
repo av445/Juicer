fc80824fd1dc3a72614da7af87af00f1583c38cc
TCG-MrFrog <linus.barger44@gmail.com>
2021-10-21 12:14:35 +0200

    fixed and added to menu and mouse

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos;
+    }
+}
