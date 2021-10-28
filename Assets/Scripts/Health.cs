851d9cd51c51052a283103b63f563b2b848063c0
OnlyDrinkTea <thea.nilssoneriksson@elev.ga.lbs.se>
2021-10-28 09:04:22 +0200

    health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Health : MonoBehaviour
 {
+
+    public int health;
+    public int nrHarts;
+
+    public Image[] harts;
+    public Sprite fullHart;
+    public Sprite emptyHart;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,18 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < harts.Length; i++)
+        {
+            if( i < nrHarts)
+            {
+                harts[i].enabled = true;
+            }
+            else
+            {
+                harts[i].enabled = false;
+            }
+        }
+
+
     }
 }
