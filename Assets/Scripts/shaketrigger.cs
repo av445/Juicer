7f8d752b79d3fc584e747ecd51136e436029c89f
nohaaa-aaa <79455783+nohaaa-aaa@users.noreply.github.com>
2021-10-28 09:48:32 +0200

    camera shake

---
+++ Assets/Scripts/shaketrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shaketrigger : MonoBehaviour
+{
+    public camerashake camerashake;
+    void Update()
+    {
+        
+    }
+}
