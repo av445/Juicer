4b8f0b84d672d44c392ea5f3ba137adcb48f0d84
TCG-MrFrog <linus.barger44@gmail.com>
2021-11-02 23:23:28 +0100

    Fixes
    I added back stuff that dissapeared last time and allso added some shit to make it look better

--- Assets/Scripts/Secret.cs
+++ Assets/Scripts/Secret.cs
@@ -6,17 +6,34 @@ public class Secret : MonoBehaviour
 {
     //This is my script for my litle secret... no peaking ;D
 
-    //These 
+    //These make it so I can change to another camera
     public GameObject camera1;
     public GameObject camera2;
 
+    public Animator Shhh;
+
+    public GameObject TheEnd;
+
+    public float countDown = 40f;
     public void Start()
     {
+        TheEnd.SetActive(false);
         camera2.SetActive(false);
     }
     public void Textsecret()
     {
         camera2.SetActive(true);
         camera1.SetActive(false);
+        Shhh.SetTrigger("Shhh");
+
+        countDown -= Time.deltaTime;
+    }
+
+    public void FixedUpdate()
+    {
+        if (countDown == 0)
+        {
+            TheEnd.SetActive(true);
+        }
     }
 }
